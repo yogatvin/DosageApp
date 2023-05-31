e51b688953c6a3e7872f92a31b1f8adf824f4e04
yogatvin <131861746+yogatvin@users.noreply.github.com>
2023-05-31 13:52:48 +0200

    Update DosageAid.py

--- DosageAid.py
+++ DosageAid.py
@@ -19,6 +19,7 @@ USERS = [
 
 def login():
     st.title("Unlock the DosageAid-App: Your Personal Medication Assistant! 🥳")
+    
     username = st.text_input("Benutzername")
     password = st.text_input("Passwort", type="password")
     login_button = st.button("Einloggen")
